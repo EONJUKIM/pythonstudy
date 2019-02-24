cca7466e4f74cfb36735b380d12aa21a337c5663
EONJUKIM <metis6072@gmail.com>
2019-02-24 10:39:13 +0900

    python study 2

---
+++ feb0224_3.py
@@ -0,0 +1,10 @@
+int_data=1
+float_data=3.14
+complex_data=1+5j
+str_data1='I love Python'
+str_data2="반갑습니다"
+list_data=[1,2,3]
+tuple_data=(1,2,3)
+dict_data={0:'False',1:'True'}
+
+print(int_data)
---
+++ putdata.py
@@ -0,0 +1,5 @@
+number=1
+pi=3.14
+flag=True
+char='x'
+chars='I love Python'
