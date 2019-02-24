baccd0a3592032f70354b6d2433bb8f25eaf72d4
EONJUKIM <metis6072@gmail.com>
2019-02-24 11:53:31 +0900

    python study 2

---
+++ printnew.py
@@ -0,0 +1,12 @@
+a=200
+msg='I love Python'
+list_data=['a','b','c']
+dict_data={'a':97,'b':98}
+print(a)
+print(msg)
+print(list_data)
+print(dict_data)
+print(dict_data['a'])
+
+print('#',end='')
+print('#')
