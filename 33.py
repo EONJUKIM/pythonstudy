820117a289c3c0c3258a4a1bc64cc45cefb31fe1
EONJUKIM <metis6072@gmail.com>
2019-03-10 11:54:39 +0900

    python study

---
+++ 33.py
@@ -0,0 +1,9 @@
+strdata1 = '나는 파이썬 프로그래머이다'
+strdata2 = "You are programmer"
+strdata3 = """I love 
+    python. You love
+python too!
+"""
+
+strdata4 = "My son's name is John"
+strdata5 = '문자열 "abc"의 길이는 3입니다.'
